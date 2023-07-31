455a59d386230472560523c87a388c9fbefe7729
杰克有茶 <chenwansal1@163.com>
2023-07-31 22:43:45 +0800

    <feature> Frame.Setup add: Window-Draw

--- Assets/com.gamearki.framework.setup/Editor/SetupEditorWindow.cs
+++ Assets/com.gamearki.framework.setup/Editor/SetupEditorWindow.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEditor;
 using UnityEditor.PackageManager;
 
-namespace GameArki.Setup {
+namespace GameArki.Setup.Editors {
 
     public static class SetupEditorWindowMenu {
 
@@ -20,8 +20,14 @@ namespace GameArki.Setup {
     // 1. 比对项目中的 manifest.json 和 
     public class SetupEditorWindow : EditorWindow {
 
-        void OnEnable() {
+        SetupSo so;
+        Vector2 scrollPos;
 
+        void OnEnable() {
+            so = AssetDatabase.LoadAssetAtPath<SetupSo>("Assets/com.gamearki.framework.setup/Editor/SetupResource/SetupSo.asset");
+            if (so == null) {
+                Debug.LogError("Can't find SetupSo.asset");
+            }
         }
 
         void OnDisable() {
@@ -29,7 +35,48 @@ namespace GameArki.Setup {
         }
 
         void OnGUI() {
+            if (so == null) {
+                return;
+            }
 
+            scrollPos = GUILayout.BeginScrollView(scrollPos);
+
+            // One SetupModel
+            for (int i = 0; i < so.all.Count; i += 1) {
+                var model = so.all[i];
+                GUI_DrawOneSetupModel(model);
+            }
+
+            GUILayout.EndScrollView();
+        }
+
+        void GUI_DrawOneSetupModel(SetupModel model) {
+
+            GUILayout.Box("fdsaf", GUILayout.ExpandWidth(true), GUILayout.Height(2));
+            GUILayout.BeginVertical();
+            // FontSize: 20
+            GUILayout.Label("包名: " + model.packageName + " " + model.version, new GUIStyle() { fontSize = 16, normal = new GUIStyleState() { textColor = Color.white }, contentOffset = new Vector2(6, 0) });
+            GUILayout.Label("描述: " + model.desc);
+
+            // One SetupModel's dirs
+            for (int i = 0; i < model.dirs.Count; i += 1) {
+                var dir = model.dirs[i];
+                GUI_DrawOneDir(dir);
+            }
+            GUILayout.EndVertical();
+
+            GUILayout.Space(5);
+
+        }
+
+        void GUI_DrawOneDir(string dir) {
+            // Align: Right
+            GUILayout.BeginHorizontal();
+
+            // One SetupModel's dir
+            GUILayout.Button(dir, GUILayout.Width(100));
+
+            GUILayout.EndHorizontal();
         }
 
     }
