67f1c1cb3323d18d62101d6fc6908ed81a930e62
Jackutea <chenwansal1@163.com>
2023-12-11 21:50:23 +0800

    <feature> FPMath add: Minkowski Difference

--- Assets/com.gamearki.fpmath/Runtime/FPMinkowski.cs
+++ Assets/com.gamearki.fpmath/Runtime/FPMinkowski.cs
@@ -6,29 +6,29 @@ namespace GameArki.FPMath {
 
     public static class FPMinkowski2D {
 
-        public static int Sum_CulledPolygon(Vector2[] aVectors, Vector2[] bVectors, Vector2[] culledPolygon) {
+        /// <summary> return the count of the result polygon </summary>
+        public static int Sum_CulledPolygon(Span<Vector2> aConvex, Span<Vector2> bConvex, Vector2[] culledPolygon) {
 
             int resCount = 0;
 
-            // A ∪ B
-            int aLen = aVectors.Length;
-            int bLen = bVectors.Length;
+            int aLen = aConvex.Length;
+            int bLen = bConvex.Length;
 
-            int aBottomIndex = BottomIndex(aVectors);
-            int bBottomIndex = BottomIndex(bVectors);
-            culledPolygon[resCount++] = aVectors[aBottomIndex] + bVectors[bBottomIndex];
+            int aBottomIndex = BottomIndex(aConvex);
+            int bBottomIndex = BottomIndex(bConvex);
+            culledPolygon[resCount++] = aConvex[aBottomIndex] + bConvex[bBottomIndex];
             int aCurIndex = (aBottomIndex + 1) % aLen;
             int bCurIndex = (bBottomIndex + 1) % bLen;
 
             while (resCount < aLen + bLen) {
 
-                culledPolygon[resCount] = aVectors[aCurIndex] + bVectors[bCurIndex];
+                culledPolygon[resCount] = aConvex[aCurIndex] + bConvex[bCurIndex];
                 resCount += 1;
 
                 int aNextIndex = (aCurIndex + 1) % aLen;
                 int bNextIndex = (bCurIndex + 1) % bLen;
 
-                float cross = Cross(aVectors[aNextIndex] - aVectors[aCurIndex], bVectors[bNextIndex] - bVectors[bCurIndex]);
+                float cross = Cross(aConvex[aNextIndex] - aConvex[aCurIndex], bConvex[bNextIndex] - bConvex[bCurIndex]);
 
                 if (cross >= 0 && aCurIndex != aBottomIndex) {
                     aCurIndex = aNextIndex;
@@ -42,11 +42,23 @@ namespace GameArki.FPMath {
             return resCount;
         }
 
+        /// <summary> return the count of the result polygon </summary>
+        public static int Diff_CulledPolygon(Span<Vector2> aVectors, Span<Vector2> bVectors, Vector2[] culledPolygon) {
+
+            Span<Vector2> bReversed = stackalloc Vector2[bVectors.Length];
+            for (int i = 0; i < bVectors.Length; i += 1) {
+                bReversed[i] = -bVectors[i];
+            }
+
+            return Sum_CulledPolygon(aVectors, bReversed, culledPolygon);
+
+        }
+
         static float Cross(Vector2 a, Vector2 b) {
             return a.x * b.y - a.y * b.x;
         }
 
-        static int BottomIndex(Vector2[] vectors) {
+        static int BottomIndex(Span<Vector2> vectors) {
             int res = 0;
             for (int i = 0; i < vectors.Length; i += 1) {
                 if (vectors[i].y < vectors[res].y || (vectors[i].y == vectors[res].y && vectors[i].x < vectors[res].x)) {
@@ -56,16 +68,16 @@ namespace GameArki.FPMath {
             return res;
         }
 
-        public static int Sum_FullPolygon(Vector2[] aVectors, Vector2[] bVectors, Vector2[] fullPolygon) {
-            int resIndex = 0;
-            for (int i = 0; i < aVectors.Length; i += 1) {
-                for (int j = 0; j < bVectors.Length; j += 1) {
-                    fullPolygon[resIndex] = aVectors[i] + bVectors[j];
-                    resIndex += 1;
-                }
-            }
-            return resIndex;
-        }
+        // public static int Sum_FullPolygon(Vector2[] aVectors, Vector2[] bVectors, Vector2[] fullPolygon) {
+        //     int resIndex = 0;
+        //     for (int i = 0; i < aVectors.Length; i += 1) {
+        //         for (int j = 0; j < bVectors.Length; j += 1) {
+        //             fullPolygon[resIndex] = aVectors[i] + bVectors[j];
+        //             resIndex += 1;
+        //         }
+        //     }
+        //     return resIndex;
+        // }
 
     }
 
--- Assets/com.gamearki.fpmath/Sample/Sample_FPMinkowski.cs
+++ Assets/com.gamearki.fpmath/Sample/Sample_FPMinkowski.cs
@@ -10,7 +10,7 @@ namespace GameArki.FPMath.Sample {
         [SerializeField] Vector2[] bVectors;
         Vector2[] results;
 
-        void Start() {
+        void OnEnable() {
             results = new Vector2[1000];
         }
 
@@ -27,7 +27,7 @@ namespace GameArki.FPMath.Sample {
                 Vector2 curP = aVectors[i];
                 Vector2 nextP = aVectors[(i + 1) % aVectors.Length];
                 Gizmos.DrawLine(curP, nextP);
-            }
+            } 
             // Draw B
             Gizmos.color = Color.green;
             for (int i = 0; i < bVectors.Length; i++) {
@@ -35,14 +35,26 @@ namespace GameArki.FPMath.Sample {
                 Vector2 nextP = bVectors[(i + 1) % bVectors.Length];
                 Gizmos.DrawLine(curP, nextP);
             }
-            // Draw Result
-            int resultLength = FPMinkowski2D.Sum_CulledPolygon(aVectors, bVectors, results);
+
+            // Draw Sum
+            int sumLength = FPMinkowski2D.Sum_CulledPolygon(aVectors, bVectors, results);
+            Gizmos.color = Color.yellow;
+            for (int i = 0; i < sumLength; i++) {
+                Vector2 point = results[i];
+                Vector2 next = results[(i + 1) % sumLength];
+                Gizmos.DrawLine(point, next);
+            }
+
+            // Draw Diff
+            int resultLength = FPMinkowski2D.Diff_CulledPolygon(bVectors, aVectors, results);
             Gizmos.color = Color.red;
             for (int i = 0; i < resultLength; i++) {
                 Vector2 point = results[i];
                 Vector2 next = results[(i + 1) % resultLength];
                 Gizmos.DrawLine(point, next);
             }
+
+            Gizmos.DrawSphere(Vector2.zero, 1f);
         }
     }
 }
